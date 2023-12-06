8585c789f2bc1229dbe6e1c399a254b2b9e0aed3
Enrique Galeano <enriquej999@gmail.com>
2023-12-05 23:20:40 -0600

    done

---
+++ practica1.js
@@ -0,0 +1,11 @@
+let edad = 24;
+const nombre = 'Enrique';
+let estudiante = true;
+
+function numeroDoble (numberEdad){
+    return numberEdad *2; 
+}
+
+let calculoMatematico = numeroDoble(edad);
+
+console.log(nombre + ' ' + calculoMatematico+' ' + estudiante);
